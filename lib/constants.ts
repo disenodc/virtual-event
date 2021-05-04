340c3728a2cd0732929678aee5d36fd88eaa6d39
disenodc <disenodc@gmail.com>
2021-05-04 12:29:32 -0300

    editando inicio

--- lib/constants.ts
+++ lib/constants.ts
@@ -21,23 +21,23 @@ export const BRAND_NAME = 'ACME';
 export const SITE_NAME_MULTILINE = ['ACME', 'Conf'];
 export const SITE_NAME = 'ACME Conf';
 export const META_DESCRIPTION =
-  'This is an open source demo that Next.js developers can clone, deploy, and fully customize for events. Created through collaboration of marketers, designers, and developers at Vercel.';
+  'Esta es una demostración de código abierto que los desarrolladores de Next.Js pueden clonar, implementar y personalizar completamente para eventos.Creado a través de la colaboración de comercializadores, diseñadores y desarrolladores en Vercel..';
 export const SITE_DESCRIPTION =
-  'An interactive online experience by the community, free for everyone.';
-export const DATE = 'October 27, 2020';
+  'Una experiencia interactiva en línea de la comunidad, gratis para todos.';
+export const DATE = 'October 27, 2021';
 export const SHORT_DATE = 'Oct 27 - 9:00am PST';
-export const FULL_DATE = 'Oct 27th 9am Pacific Time (GMT-7)';
+export const FULL_DATE = 'Oct 27th 9am Pacific Time (GMT-3)';
 export const TWEET_TEXT = META_DESCRIPTION;
 export const COOKIE = 'user-id';
 
 // Remove process.env.NEXT_PUBLIC_... below and replace them with
 // strings containing your own privacy policy URL and copyright holder name
-export const LEGAL_URL = process.env.NEXT_PUBLIC_PRIVACY_POLICY_URL;
-export const COPYRIGHT_HOLDER = process.env.NEXT_PUBLIC_COPYRIGHT_HOLDER;
+// export const LEGAL_URL = process.env.NEXT_PUBLIC_PRIVACY_POLICY_URL;
+// export const COPYRIGHT_HOLDER = process.env.NEXT_PUBLIC_COPYRIGHT_HOLDER;
 
-export const CODE_OF_CONDUCT =
-  'https://www.notion.so/vercel/Code-of-Conduct-Example-7ddd8d0e9c354bb597a0faed87310a78';
-export const REPO = 'https://github.com/vercel/virtual-event-starter-kit';
+//export const CODE_OF_CONDUCT =
+//   'https://www.notion.so/vercel/Code-of-Conduct-Example-7ddd8d0e9c354bb597a0faed87310a78';
+// export const REPO = 'https://github.com/vercel/virtual-event-starter-kit';
 export const SAMPLE_TICKET_NUMBER = 1234;
 export const NAVIGATION = [
   {
